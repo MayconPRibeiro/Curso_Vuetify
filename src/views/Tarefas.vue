e5152b2ce9cd35f30ca7b7811264881d55517993
MayconPRibeiro <maycon_ribeiro10@hotmail.com>
2026-07-28 18:34:36 -0300

    Adicionar funcionalidade para adicionar tarefas e exibir títulos corretamente

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="home">
     <v-list flat subheader>
+      <v-col cols="12" sm="6">
+        <v-text-field
+          v-model="campoInput"
+          label="Digite uma nova tarefa"
+          clearable
+          @keyup.enter="handleAddTarefa"
+        ></v-text-field>
+      </v-col>
+
       <v-subheader>Todas as tarefas</v-subheader>
 
-      <v-list-item-group v-model="settings" multiple active-class="">
-        <Tarefa />
-        <Tarefa />
-        <Tarefa />
+      <v-list-item-group multiple active-class="">
+        <div v-for="(tarefa, index) in tarefas" :key="index">
+          <Tarefa :tarefa="tarefa" />
+        </div>
       </v-list-item-group>
     </v-list>
   </div>
@@ -19,6 +28,26 @@ export default {
   name: "Home",
   components: {
     Tarefa,
+  },
+  data() {
+    return {
+      campoInput: null,
+      tarefas: [
+        { titulo: "Ir ao mercado", concluido: false },
+        { titulo: "Comprar ração", concluido: false },
+      ],
+    };
+  },
+  methods: {
+    handleAddTarefa() {
+      if (this.campoInput) {
+        this.tarefas.push({
+          titulo: this.campoInput,
+          consluido: false,
+        });
+        this.campoInput = null;
+      }
+    },
   },
 };
 </script>
